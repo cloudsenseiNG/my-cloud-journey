bdff9e73f566ba16679724f60b7d6fec5a056172
mayurwaB <bodlexnig@outlook.com>
2023-01-18 03:48:17 +0000

    updates kubernetes-commands

--- kubernetes/kubernetes-commands.sh
+++ kubernetes/kubernetes-commands.sh
@@ -5,13 +5,13 @@ kubectl annotate pod pod-name annnotation-key=annotaion-value #annotates a speci
 #
 ####Namespacing
 kubectl get ns #this lists available namespaces
-kubectl get po --namespace namespace #get pods in a specified namespace
-kubectl create namespace custom-namespace #creates a namespace
-kubectl create -f kubia-manual.yaml -n custom-namespace #creates pod in specified namespace
+kubectl get pod --namespace namespace-name #get pods in a specified namespace
+kubectl create namespace namespace-name #creates a namespace
+kubectl create -f file-name.yaml -n namespace #creates pod in specified namespace
 #
 ####Deleting pods
-kubectl delete pod kubia-gpu #deleting pods by name
-kubectl delete pod -l creation_method=manual #deleting pods by label
-kubectl delete ns custom-namespace #deleting a namespace and all its resources
+kubectl delete pod pod-name #deleting pods by name
+kubectl delete pod -l key=value #deleting pods by label
+kubectl delete ns namespace #deleting a namespace and all its resources
 kubectl delete pod --all #deleting all pods in the current namespace
 kubectl delete all --all #deleting all resources in the current namespace
