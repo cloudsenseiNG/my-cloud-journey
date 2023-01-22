d023a791b9ddedff54b735ce26ab8faff18c45de
Mayowa Bodunwa <bodlexnig@outlook.com>
2023-01-22 13:18:02 +0100

    Create ubuntu-kube-setup.sh

---
+++ kubernetes/ubuntu-kube-setup.sh
@@ -0,0 +1,26 @@
+#Install docker
+sudo apt-get update -y
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#Add user to docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+#Download and start minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start 1> /dev/null
+alias kubectl="minikube kubectl --"
+echo "Try your first kubectl command. Ex: kubectl get pods"
