0c072c9f553420c3e90bc5378becd88d32af66bd
Mayowa Bodunwa <bodlexnig@outlook.com>
2023-01-22 10:34:44 +0100

    Update git-ssh-setup.sh

--- git/git-ssh-setup.sh
+++ git/git-ssh-setup.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-#
+
+#Color_Variables
+
+red="\033[31;3m"
+no_color="\033[0m"
+
+
+#Reads text input
 read -p "Enter your email: " email
-ssh-keygen -t ed25519 -C "${email}"
+
+echo -e "Supported Key Types: ${red}\ndsa \necdsa \necdsa-sk \ned25519 \ned25519-sk \nrsa${no_color}"
+
+read -p "Enter your key type: " type
+
+#Generates SSH key
+ssh-keygen -t ${type} -C "${email}"
+
+#Starts SSH agent with output
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519.pub
-#ssh -vT git@github.com
+
+#Adds private key to ssh-agent
+ssh-add ~/.ssh/id_${type}
+
+#Prints public key
+echo -e "Copy this key: $red $(cat ~/.ssh/id_${type}.pub) $no_color and add to GitHub SSH keys"
